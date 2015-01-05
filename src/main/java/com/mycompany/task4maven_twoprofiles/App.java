f403fbb74ae219736eb2e7f54b8e44f2b9c1bb20
Dmitriy Malygin <dmitrima10@gmail.com>
2015-01-05 20:45:26 +0300

    First commit

---
+++ src/main/java/com/mycompany/task4maven_twoprofiles/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.task4maven_twoprofiles;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
